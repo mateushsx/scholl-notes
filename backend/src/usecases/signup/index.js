bd4afff28c36ff4ae7c6e3decc780a0ddc2f005e
Mateus Henrique <mateusdzn10@gmail.com>
2023-01-29 17:32:13 -0400

    feat: add use case to sign up user

---
+++ backend/src/usecases/signup/index.js
@@ -0,0 +1,43 @@
+import crypto from 'crypto';
+
+import {
+    isValidName,
+    isValidEmail,
+    isValidPassword,
+} from '../../shared/validators/index.js';
+import {AlreadyExistsError, MissingParamError} from '../errors/index.js';
+
+export class SignupUseCase {
+    constructor(userDataAccess, encrypter, tokenManager) {
+        this.userDataAccess = userDataAccess;
+        this.encrypter = encrypter;
+        this.tokenManager = tokenManager;
+    }
+
+    async execute(user) {
+        if (!isValidName(user.name)) {
+            throw new MissingParamError('name');
+        }
+        if (!isValidEmail(user.email)) {
+            throw new MissingParamError('email');
+        }
+        if (!isValidPassword(user.password)) {
+            throw new MissingParamError('password');
+        }
+
+        const userExists = await this.userDataAccess.findByEmail(user.email);
+
+        if (userExists) {
+            throw new AlreadyExistsError('user');
+        }
+
+        const hash = await this.encrypter.encrypt(user.password);
+        user.password = hash;
+        user.id = crypto.randomUUID();
+
+        const {id} = await this.userDataAccess.save(user);
+        const token = await this.tokenManager.sign({key: {id}});
+
+        return {id, token};
+    }
+}
