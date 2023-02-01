19b87aa2cf7631184d3dd2d947f77f82490edf16
Mateus Henrique <mateusdzn10@gmail.com>
2023-01-31 20:53:05 -0400

    Revert "refactor: move user controllers"
    This reverts commit b2e31cf3c2bd1654be492f278839c30ea9e2ea9d.

---
+++ backend/src/controllers/signin/index.js
@@ -0,0 +1,20 @@
+export class SigninController {
+    constructor(signinUseCase) {
+        this.signinUseCase = signinUseCase;
+    }
+
+    async handler({request, response}) {
+        try {
+            const payload = request.body;
+            if (JSON.stringify(payload) === '{}') {
+                return response.status(204).json({message: 'payload is empty'});
+            }
+
+            const result = await this.signinUseCase.execute(payload);
+
+            return response.status(202).send(result);
+        } catch (error) {
+            return response.send(error);
+        }
+    }
+}
