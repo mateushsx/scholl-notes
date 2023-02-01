eb85076fe99aa116ced131a95a7d40069def4dcb
Mateus Henrique <mateusdzn10@gmail.com>
2023-01-31 21:38:03 -0400

    feat: add matter data access

---
+++ backend/src/data/matter/index.js
@@ -0,0 +1,15 @@
+import {MatterModel} from '../../models/matter/index.js';
+
+export class MatterDataAccess {
+    async save(matterInput) {
+        const matter = new MatterModel(matterInput);
+        const matterData = await matter.save();
+
+        return {id: matterData.id};
+    }
+    async findById(id) {
+        const matter = await MatterModel.findOne({id});
+        if (matter && matter.id) return matter;
+        return null;
+    }
+}
