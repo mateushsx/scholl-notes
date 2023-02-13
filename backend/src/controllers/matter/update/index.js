6a52efb7a30ca06a2066b40daac7558ca5af06a4
Mateus Henrique <mateusdzn10@gmail.com>
2023-02-13 06:55:33 -0400

    feat: add param id in execute method of use case

--- backend/src/controllers/matter/update/index.js
+++ backend/src/controllers/matter/update/index.js
@@ -6,11 +6,13 @@ export class UpdateMatterController {
     async handler({request, response}) {
         try {
             const payload = request.body;
+            const {id} = request.params;
+
             if (JSON.stringify(payload) === '{}') {
                 return response.status(204).json({message: 'payload is empty'});
             }
 
-            const result = await this.updateMatterUseCase.execute(payload);
+            const result = await this.updateMatterUseCase.execute(id, payload);
 
             return response.status(202).send(result);
         } catch (error) {
