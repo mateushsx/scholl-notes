5b604b695c2a45f211fa073170d12474019be068
Mateus Henrique <mateusdzn10@gmail.com>
2023-02-14 07:09:11 -0400

    feat: add auth middleware on the routes of matters

--- backend/src/routes/matter/index.js
+++ backend/src/routes/matter/index.js
@@ -2,12 +2,15 @@ import {listMatterFactory} from '../../main/factories/matter/list-factory.js';
 import {registerMatterFactory} from '../../main/factories/matter/register-factory.js';
 import {updateMatterFactory} from '../../main/factories/matter/update-factory.js';
 import {deleteMatterFactory} from '../../main/factories/matter/delete-factory.js';
+import {authMiddleware} from '../../middlewares/auth-handler.js';
 
 /**
  *
  * @param {import('fastify').FastifyInstance} app
  */
 export async function matterRoutes(app) {
+    app.addHook('preHandler', authMiddleware);
+
     app.post('/matter/register', registerMatterFactory);
     app.post('/matter/list', listMatterFactory);
     app.put('/matter/update/:id', updateMatterFactory);
