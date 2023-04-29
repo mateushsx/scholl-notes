d330fbc85b023543ce9b30076d29e17434c506a8
Mateus Henrique <mateusdzn10@gmail.com>
2023-04-29 01:18:18 -0400

    feat: add global styles

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    body {
+        font-family: 'Poppins', Tahoma, Geneva, Verdana, sans-serif;
+    }
+`;
