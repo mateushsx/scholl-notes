9f2f8d3aa9f59eabbf399aa9cfbff97e70c507a4
Mateus Henrique <mateusdzn10@gmail.com>
2023-04-29 01:19:27 -0400

    feat: add global styles in main file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './App';
+import React from "react";
+import { App } from "./App";
+import ReactDOM from "react-dom/client";
+import { GlobalStyles } from "./styles/global";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
+        <GlobalStyles />
     </React.StrictMode>
 );
