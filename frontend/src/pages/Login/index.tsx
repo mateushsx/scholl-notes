9978e63ac20be8917f075f57f2175d82b01b9d69
Mateus Henrique <mateusdzn10@gmail.com>
2023-04-29 01:19:46 -0400

    feat: add login page

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,3 +1,70 @@
+import { useState } from 'react';
+import {
+    LoginBody,
+    LoginLogo,
+    LoginHeader,
+    LoginContent,
+    LoginContainer,
+    LoginTextField,
+    LoginLabel,
+    LoginInput,
+    LoginButton,
+} from './styles';
+import { validateEmail } from '../../validators';
+
 export function Login(): JSX.Element {
-    return <div>Under development</div>;
+    const [email, setEmail] = useState<string>('');
+    const [emailError, setEmailError] = useState<boolean>(false);
+    const [password, setPassword] = useState<string>('');
+    const [passwordError, setPasswordError] = useState<boolean>(false);
+
+    const handleEmail = (event: { target: { value: string } }) => {
+        const targetValue = event.target.value;
+        const { isValid, value } = validateEmail({ email: targetValue });
+
+        setEmailError(!isValid);
+        setEmail(value);
+    };
+
+    const handlePassword = (event: { target: { value: string } }) => {
+        const targetValue = event.target.value;
+        const isValid = targetValue && targetValue.length >= 8;
+
+        setPasswordError(!isValid);
+        setPassword(targetValue);
+    };
+
+    return (
+        <LoginContainer>
+            <LoginContent>
+                <LoginHeader>
+                    <LoginLogo src="./src/assets/images/school-notes-logo.png" />
+                </LoginHeader>
+
+                <LoginBody>
+                    <LoginTextField>
+                        <LoginLabel>Email</LoginLabel>
+                        <LoginInput
+                            value={email}
+                            onChange={handleEmail}
+                            placeholder="ex: exemplo@email.com"
+                            error={emailError}
+                        />
+                    </LoginTextField>
+
+                    <LoginTextField>
+                        <LoginLabel>Senha</LoginLabel>
+                        <LoginInput
+                            value={password}
+                            onChange={handlePassword}
+                            placeholder="ex: 12345678"
+                            error={passwordError}
+                        />
+                    </LoginTextField>
+
+                    <LoginButton>Entrar</LoginButton>
+                </LoginBody>
+            </LoginContent>
+        </LoginContainer>
+    );
 }
